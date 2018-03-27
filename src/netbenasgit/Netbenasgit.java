3835e0fae7f7b464da0ec935932bb3bc381a6eb1
otro <otro@devil-laptop7>
2018-03-27 13:23:05 -0600

    adios mundo

--- src/netbenasgit/Netbenasgit.java
+++ src/netbenasgit/Netbenasgit.java
@@ -17,7 +17,7 @@ public class Netbenasgit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Adios mundo");
     }
     
 }
